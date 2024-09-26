18cfd2481b4feb7dc718318f2abdefc80c1c5cb2
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-09-26 10:16:03 -0500

    Course selection

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './CourseList.css';
 
-const CourseList = ({ courses, selectedTerm }) => {
+const CourseList = ({courses, selected, toggleSelected, selectedTerm }) => {
   const filteredCourses = Object.entries(courses).filter(([key, course]) => course.term === selectedTerm);
 
   return (
     <div className="course-list">
       {filteredCourses.map(([key, course]) => (
-        <div className="card m-2 p-2 shadow" key={key}>
+        <div
+          className={`card m-2 p-2 shadow ${selected.includes(key) ? "selected" : ""}`}
+          key={key}
+          onClick={() => toggleSelected(key)}
+        >
           <div className="card-body">
             <h5 className="card-title">{course.term} CS {course.number}</h5>
             <p className="card-text">{course.title}</p>
--- src/components/TermPage.jsx
+++ src/components/TermPage.jsx
@@ -4,11 +4,24 @@ import CourseList from './CourseList';
 
 const TermPage = ({ courses }) => {
   const [selectedTerm, setSelectedTerm] = useState('Fall');
+  const [selectedCourses, setSelectedCourses] = useState([]);
+
+  const toggleSelected = (key) => {
+    setSelectedCourses(selectedCourses.includes(key)
+      ? selectedCourses.filter(courseKey => courseKey !== key)
+      : [...selectedCourses, key]
+    );
+  }
 
   return (
     <div>
       <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-      <CourseList courses={courses} selectedTerm={selectedTerm} />
+      <CourseList
+        courses={courses}
+        selected={selectedCourses}
+        toggleSelected={toggleSelected}
+        selectedTerm={selectedTerm}
+      />
     </div>
   );
 };
